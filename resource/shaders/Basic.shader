1c42329744ce1e18a806c7434993c1c32ea8db77
Shubham Raj <shubhamxraj10@gmail.com>
2026-03-30 22:22:33 +0530

    feat: move model view matrix multiplication to shaders

--- resource/shaders/Basic.shader
+++ resource/shaders/Basic.shader
@@ -6,11 +6,14 @@ layout (location = 1) in vec2 texCoord;
 
 
 out vec2 v_TexCoord;
-uniform mat4 u_MVP;
+
+uniform mat4 u_Proj;
+uniform mat4 u_View;
+uniform mat4 u_Model;
 
 void main()
 {
-    gl_Position = u_MVP * position;
+    gl_Position = u_Proj * u_View * u_Model * position;
     v_TexCoord = texCoord;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/matrix_transform.hpp"
 #include <Shader.hpp>
 #include <Texture.hpp>
 #include <VertexBufferLayout.hpp>
@@ -14,17 +15,17 @@
 #include <stdlib.h>
 
 float positions[16] = {
-    -0.5f, -0.5f, 0.0f, 0.0f, // bottom left
-    0.5f,  -0.5f, 1.0f, 0.0f, // bottom right
-    0.5f,  0.5f,  1.0f, 1.0f, // top
-    -0.5f, 0.5f,  0.0f, 1.0f  // top left
+    100.0f, 100.0f, 0.0f, 0.0f, // bottom left
+    200.0f, 100.0f, 1.0f, 0.0f, // bottom right
+    200.0f, 200.0f, 1.0f, 1.0f, // top
+    100.0f, 200.0f, 0.0f, 1.0f  // top left
 };
 
 unsigned int indices[6] = {0, 1, 2, 2, 3, 0};
 
 int main(void) {
   {
-    Window window(800, 600, "OpenGl Window");
+    Window window(960, 540, "OpenGl Window");
     window.HandleWindowResize();
     window.PrintInfo();
 
@@ -39,13 +40,20 @@ int main(void) {
     layout.Push<float>(2);
     va.AddBuffer(vb, layout);
 
-    glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+    glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+
+    // Camera positions;
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+    // Camera positions rotation and movement and scale;
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 100, 0));
 
     Shader shader("resource/shaders/Basic.shader");
     shader.Bind();
 
-    shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.7f, 0.8f);
-    shader.SetUniformMat4f("u_MVP", proj);
+    // doing the matrix multiplication on the gpu should be faster!!
+    shader.SetUniformMat4f("u_Proj", proj);
+    shader.SetUniformMat4f("u_View", view);
+    shader.SetUniformMat4f("u_Model", model);
 
     Texture Texture("resource/pp.jpg");
     Texture.Bind(0);
