68dcad1355216973a3bb0625c471f22e20a44aa9
Shubham Raj <shubhamxraj10@gmail.com>
2026-03-31 02:11:23 +0530

    feat: Added multiple of same object using differnet translation

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,10 @@
 #include <stdlib.h>
 
 float positions[16] = {
-    100.0f, 100.0f, 0.0f, 0.0f, // bottom left
-    200.0f, 100.0f, 1.0f, 0.0f, // bottom right
-    200.0f, 200.0f, 1.0f, 1.0f, // top
-    100.0f, 200.0f, 0.0f, 1.0f  // top left
+    -50.0f, -50.0f, 0.0f, 0.0f, // bottom left
+    50.0f,  -50.0f, 1.0f, 0.0f, // bottom right
+    50.0f,  50.0f,  1.0f, 1.0f, // top
+    -50.0f, 50.0f,  0.0f, 1.0f  // top left
 };
 
 unsigned int indices[6] = {0, 1, 2, 2, 3, 0};
@@ -34,6 +34,7 @@ int main(void) {
     window.HandleWindowResize();
     window.PrintInfo();
 
+    // Enable GL_BLENDING
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
@@ -45,16 +46,9 @@ int main(void) {
     layout.Push<float>(2);
     va.AddBuffer(vb, layout);
 
-    float viewX = -100, viewY = 0, viewZ = 0;
-    float modelX = 200, modelY = 100, modelZ = 0;
-
-    // Projection;
     glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    // Camera positions;
     Shader shader("resource/shaders/Basic.shader");
     shader.Bind();
-
-    // doing the matrix multiplication on the gpu should be faster!!
 
     Texture Texture("resource/pp.jpg");
     Texture.Bind(0);
@@ -76,16 +70,15 @@ int main(void) {
     io.ConfigFlags |=
         ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
 
-    // Setup Dear ImGui style
-
     ImGui_ImplGlfw_InitForOpenGL(window.getWindow(), true);
     ImGui_ImplOpenGL3_Init("#version 330");
     ImGui::StyleColorsDark();
 
-    glm::vec3 translationView(200, 200, 0);
-    glm::vec3 translationModel(200, 200, 0);
-    float r = 0.0f;
-    float increment = 0.05f;
+    glm::vec3 translationViewA(0, 0, 0);
+    glm::vec3 translationModelA(0, 0, 0);
+
+    glm::vec3 translationViewB(0, 0, 0);
+    glm::vec3 translationModelB(0, 0, 0);
 
     while (!window.ShouldClose()) {
       window.ProcessInput();
@@ -97,32 +90,46 @@ int main(void) {
       ImGui::NewFrame();
 
       shader.Bind();
-      // shader.SetUniform4f("u_Color", r, 0.3f, 0.7f, 0.8f);
-      glm::mat4 view = glm::translate(glm::mat4(1.0f), translationView);
-      // Camera positions rotation and movement and scale;
-      glm::mat4 model = glm::translate(glm::mat4(1.0f), translationModel);
 
-      shader.SetUniformMat4f("u_Proj", proj);
-      shader.SetUniformMat4f("u_View", view);
-      shader.SetUniformMat4f("u_Model", model);
-      renderer.Draw(va, ib, shader);
-
-      if (r > 1.0f) {
-        increment = -0.01f;
-      } else if (r < 0.0f) {
-        increment = 0.01f;
-      }
-      r += increment;
       {
-        ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!"
-                                       // and append into it.
-        ImGui::SliderFloat3(
-            "ViewTransaltion", &translationView.x, 0.0f,
-            960.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-        ImGui::SliderFloat3(
-            "ModelTranslation", &translationModel.x, 0.0f,
-            540.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-        ImGui::End();
+
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), translationViewA);
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), translationModelA);
+
+        shader.SetUniformMat4f("u_Proj", proj);
+        shader.SetUniformMat4f("u_View", view);
+        shader.SetUniformMat4f("u_Model", model);
+        renderer.Draw(va, ib, shader);
+
+        // the state control for the object 1;
+        {
+          ImGui::Begin("Object 1");
+          ImGui::SliderFloat3("ViewTransaltion", &translationViewA.x, 0.0f,
+                              960.0f);
+          ImGui::SliderFloat3("ModelTranslation", &translationModelA.x, 0.0f,
+                              960.0f);
+          ImGui::End();
+        }
+      }
+
+      {
+
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), translationViewB);
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), translationModelB);
+        shader.SetUniformMat4f("u_Proj", proj);
+        shader.SetUniformMat4f("u_View", view);
+        shader.SetUniformMat4f("u_Model", model);
+        renderer.Draw(va, ib, shader);
+
+        // the state control for the object 2;
+        {
+          ImGui::Begin("Object 2");
+          ImGui::SliderFloat3("ViewTransaltion", &translationViewB.x, 0.0f,
+                              960.0f);
+          ImGui::SliderFloat3("ModelTranslation", &translationModelB.x, 0.0f,
+                              960.0f);
+          ImGui::End();
+        }
       }
 
       ImGui::Render();
@@ -131,6 +138,7 @@ int main(void) {
       window.SwapBuffer();
       window.PollEvents();
     }
+
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
