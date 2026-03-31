ac7dd4e0a2f26291499edb940fa54edb085a176b
Shubham Raj <shubhamxraj10@gmail.com>
2026-03-31 21:05:07 +0530

    feat: added basic water shader test

--- resource/shaders/water.shader
+++ resource/shaders/water.shader
@@ -6,6 +6,7 @@ layout (location = 1) in vec2 texCoord;
 
 
 out vec2 v_TexCoord;
+out float v_Time;
 
 uniform mat4 u_Proj;
 uniform mat4 u_View;
@@ -18,13 +19,14 @@ void main()
 
     float time = u_Time;
     //wave
-    pos.y += sin(pos.x*2.0 + time) * 0.2;
-    pos.y += cos(pos.z*2.0 + time) * 0.2;
+    pos.y += sin(pos.x*2.0 + time) * 2;
+    pos.y += cos(pos.z*2.0 + time) * 2;
 
     // MVP translation
     vec4 final = u_Proj * u_View * u_Model * vec4(pos, 1.0);
     gl_Position = final;
     v_TexCoord = texCoord;
+    v_Time = time;
 }
 
 #shader fragment
@@ -34,12 +36,20 @@ void main()
 layout (location = 0) out vec4 color;
 
 in vec2 v_TexCoord;
+in float v_Time;
 
 uniform vec4 u_Color;
 uniform sampler2D u_Texture;
+uniform vec4 u_Freq_Amp;
 
 void main()
 {
-    vec4 texColor = texture(u_Texture, v_TexCoord);
+    vec2 uv = v_TexCoord;
+    float time = v_Time;
+
+    // Wave distortion
+    uv.x += sin(uv.y * u_Freq_Amp.x + time) * u_Freq_Amp.z;
+    uv.y += cos(uv.x * u_Freq_Amp.y + time) * u_Freq_Amp.w;
+    vec4 texColor = texture(u_Texture, uv);
     color = texColor;
 };
--- src/tests/waterEffect.cpp
+++ src/tests/waterEffect.cpp
@@ -1,79 +1,82 @@
 
 #include <IndexBuffer.hpp>
 #include <VertexBuffer.hpp>
+#include <glad/glad.h>
+// Window needs to be after glad
+#include <Window.hpp>
 #include <glm/ext/matrix_clip_space.hpp>
 #include <glm/ext/matrix_transform.hpp>
-#include <glad/glad.h>
-#include <Window.hpp>
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
 #include <tests/WaterEffect.hpp>
 
-namespace test
-{
-  WaterEffect::WaterEffect()
+namespace test {
+WaterEffect::WaterEffect()
       : m_positions{
-            -50.0f, -50.0f, 0.0f, 0.0f, // bottom left
-            50.0f, -50.0f, 1.0f, 0.0f,  // bottom right
-            50.0f, 50.0f, 1.0f, 1.0f,   // top
-            -50.0f, 50.0f, 0.0f, 1.0f   // top left
+            -200.0f, -200.0f, 0.0f, 0.0f, // bottom left
+            200.0f, -200.0f, 1.0f, 0.0f,  // bottom right
+            200.0f, 200.0f, 1.0f, 1.0f,   // top
+            -200.0f, 200.0f, 0.0f, 1.0f   // top left
         },
         m_indexes{0, 1, 2, 2, 3, 0},
         m_vb(m_positions, 4 * 4 * sizeof(float)),
         m_ib(m_indexes, 6),
         m_shader("resource/shaders/water.shader"),
-        m_texture("resource/water_texture.jpg")
+        m_texture("resource/water_texture.jpg"),
+        m_Freq(glm::vec2(1,1)),
+        m_Amp(glm::vec2(0.2,0.2))
   {
-    m_layout.Push<float>(2);
-    m_layout.Push<float>(2);
-    m_va.AddBuffer(m_vb, m_layout);
+  m_layout.Push<float>(2);
+  m_layout.Push<float>(2);
+  m_va.AddBuffer(m_vb, m_layout);
 
-    m_proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+  m_proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
 
-    m_shader.Bind();
+  m_shader.Bind();
 
-    m_texture.Bind(0);
-    m_shader.SetUniform1i("u_Texture", 0);
-    m_shader.SetUniform1f("u_Time", glfwGetTime());
+  m_texture.Bind(0);
+  m_shader.SetUniform1i("u_Texture", 0);
 
-    m_va.Unbind();
-    m_vb.Unbind();
-    m_ib.Unbind();
-    m_shader.UnBind();
+  m_va.Unbind();
+  m_vb.Unbind();
+  m_ib.Unbind();
+  m_shader.UnBind();
 
-    m_translationModelA = glm::vec3(0, 0, 0);
-    m_translationViewA = glm::vec3(0, 0, 0);
-  };
+  m_translationModelA = glm::vec3(0, 0, 0);
+  m_translationViewA = glm::vec3(0, 0, 0);
+};
 
-  WaterEffect::~WaterEffect() {};
+WaterEffect::~WaterEffect() {};
 
-  void WaterEffect::onUpdate(float deltaTime)
+void WaterEffect::onUpdate(float deltaTime) {};
+
+void WaterEffect::onRender() {
+
+  m_shader.Bind();
+
   {
-    m_shader.SetUniform1f("u_Time", glfwGetTime());
+
     m_view = glm::translate(glm::mat4(1.0f), m_translationViewA);
     m_model = glm::translate(glm::mat4(1.0f), m_translationModelA);
-  };
 
-  void WaterEffect::onRender()
-  {
+    m_shader.SetUniformMat4f("u_Proj", m_proj);
+    m_shader.SetUniformMat4f("u_View", m_view);
+    m_shader.SetUniformMat4f("u_Model", m_model);
 
-    m_shader.Bind();
+    m_shader.SetUniform1f("u_Time", glfwGetTime());
+    m_shader.SetUniform4f("u_Freq_Amp", m_Freq[0], m_Freq[1], m_Amp[0],
+                          m_Amp[1]);
+    m_renderer.Draw(m_va, m_ib, m_shader);
+  }
+};
 
-    {
-      m_shader.SetUniformMat4f("u_Proj", m_proj);
-      m_shader.SetUniformMat4f("u_View", m_view);
-      m_shader.SetUniformMat4f("u_Model", m_model);
-      m_shader.SetUniform1f("u_Time", glfwGetTime());
-      m_renderer.Draw(m_va, m_ib, m_shader);
-    }
-  };
-
-  void WaterEffect::onImGuiRender()
-  {
-    ImGui::Begin("Water Effect!");
-    ImGui::SliderFloat3("ViewTransaltion", &m_translationViewA.x, 0.0f, 960.0f);
-    ImGui::SliderFloat3("ModelTranslation", &m_translationModelA.x, 0.0f, 960.0f);
-    ImGui::End();
-  };
+void WaterEffect::onImGuiRender() {
+  ImGui::Begin("Water Effect!");
+  ImGui::SliderFloat3("ViewTransaltion", &m_translationViewA.x, 0.0f, 960.0f);
+  ImGui::SliderFloat3("ModelTranslation", &m_translationModelA.x, 0.0f, 960.0f);
+  ImGui::SliderFloat2("Water Frequency", &m_Freq.x, 0.0f, 100.0f);
+  ImGui::SliderFloat2("Water Amplititude", &m_Amp.x, 0.0f, 2.0f);
+  ImGui::End();
+};
 }; // namespace test
